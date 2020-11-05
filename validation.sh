11615d0b7fd61cbeb0bd665272f58551d1d5ff61
priya-abc <priyapriya6598@gmail.com>
2020-11-05 16:24:28 +0530

    Added: Checking char length is Done

--- validation.sh
+++ validation.sh
@@ -28,3 +28,19 @@ then
 else
     echo "Email address $email is invalid."
 fi
+echo "enter phone number:"
+read number
+if [[ "$number" =~ ^\d{2}\s\d{10}$ ]]
+then
+    echo "Phone number is valid"
+else
+    echo "phone number is invalid"
+fi
+echo "enter the password"
+read password
+
+len="${#password}"
+if test $len -ge 8
+else
+    echo "password lenght should be greater than or equal 8 hence weak password"
+fi
