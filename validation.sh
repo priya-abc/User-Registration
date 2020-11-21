4251c8d2e2ff2625c0a7d4fb5d6b68f1e61fdbcb
priya-abc <priyapriya6598@gmail.com>
2020-11-21 20:04:43 +0530

    Added: Lastname added

--- validation.sh
+++ validation.sh
@@ -10,3 +10,11 @@ then
 else
     echo "First name is invalid"
 fi
+echo "enter the name:"
+read name
+if [[ "$name" =~ ^([A-Za-z]{3,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?) ]]
+then
+    echo "name is valid"
+else
+    echo "name is invalid"
+fi
