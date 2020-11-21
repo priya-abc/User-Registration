3dbf29c8647932e48bdade8d29e8764a225f8ea1
priya-abc <priyapriya6598@gmail.com>
2020-11-21 20:03:24 +0530

    Added: Firstname added

--- validation.sh
+++ validation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 ##WELCOME MESSAGE##
 echo "Welcome to user registration"
+echo "enter the name:"
+read name 
 
+if [[ "$name" =~ ^([A-Za-z]{3,})
+then
+    echo "First name is valid"
+else
+    echo "First name is invalid"
+fi
